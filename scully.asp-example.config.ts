a00f0d65dd5474f54f3ec5f65bb6bf51da349a18
Jefiozie <jefiozie.bosch@gmail.com>
2022-01-18 12:24:44 +0100

    chore: add scully to the project

---
+++ scully.asp-example.config.ts
@@ -0,0 +1,12 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+/** this loads the default render plugin, remove when switching to something else. */
+
+
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "asp-example",
+  outDir: './dist/static',
+  routes: {
+  }
+};
